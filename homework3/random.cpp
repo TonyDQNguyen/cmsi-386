89ec0c999b419770616f723757adcd8ba8ef6693
Tony Nguyen <tonydqnguyen@gmail.com>
2018-06-01 03:16:29 -0700

    Staged Changes

---
+++ homework3/random.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int x = 2;
+void f() { std::cout << x << '\n'; }
+void g() { int x = 5; f(); std::cout << x << '\n'; }
+int main() {
+  g();
+  std::cout << x << '\n';
+}
--- homework3/stack.cpp
+++ homework3/stack.cpp
@@ -6,74 +6,97 @@ using namespace std;
 template <typename T>
 class Stack {
 
-private:
+  struct Node {
+    T data;
+    Node* next;
+  };
 
-    struct Node {
-        T data;
-        Node* next;
-    };
+  int size = 0;
+  Node* top = nullptr;
 
-    int size = 0;
-    Node* top = nullptr;
+  Node* copy(Node* n) {
+    return new Node {n->data, n->next ? copy(n->next) : nullptr};
+  }
 
-    Node* copy(Node* n) {
-        return new Node {n->data,  n->next ? copy(n->next): nullptr};
-    }
 public:
 
-    ~Stack() {
-        while (top != nullptr){
-            pop();
-        }
+  ~Stack() {
+    while (top != nullptr) {
+      pop();
     }
-    Stack() = default;
+  }
 
-    //Construction
-    Stack(const Stack& s) {
-        size = s.size;
-        top = copy(s.top);
-    }
+  Stack() = default;
 
-    //Assignment
-    Stack& operator = (const Stack& s) {
-        if (&s != this){
-            while (top != nullptr) pop();
-            size = s.size;
-            top = copy(s.top);
-        }
-        return *this;
-    }
-    int get_size(){
-        return size;
-    }
+  Stack(const Stack& s) = delete;
+  Stack& operator=(const Stack& s) = delete;
 
-    T get_top(){
-        return top->data;
-    }
+  Stack(Stack&& s): size(s.size), top(s.top) {
+    s.top = nullptr;
+    s.size = 0;
+  }
 
-    void push(T x) {
-        top = new Node{x, top};
-        size++;
+  Stack& operator=(Stack&& s) {
+    if (&s != this) {
+      size = s.size;
+      top = s.top;
+      s.top = nullptr;
+      s.size = 0;
     }
+    return *this;
+  }
 
-    T pop(){
-        Node* nodeToDelete = top;
-        T valueToReturn = top->data;
-        top = top->next;
-        size--;
-        delete nodeToDelete;
-        return valueToReturn;
-    }
+  int get_size() {
+    return size;
+  }
+
+  T get_top() {
+    return top->data;
+  }
+
+  void push(T x) {
+    top = new Node {x, top};
+    size++;
+  }
+
+  T pop() {
+    Node* nodeToDelete = top;
+    T valueToReturn = top->data;
+    top = top->next;
+    size--;
+    delete nodeToDelete;
+    return valueToReturn;
+  }
 };
 
-int main(){
-    Stack<int> s;
-    assert(s.get_size() == 0);
-    s.push(100);
-    assert(s.get_top() == 100);
-    assert(s.get_size() == 1);
-    s.push(200);
-    assert(s.get_top() == 200);
-    assert(s.get_size() == 2);
+Stack<int> oneTwoThree() {
+  Stack<int> z;
+  z.push(3);
+  z.push(2);
+  z.push(1);
+  return z;
+}
 
-};
+int main() {
+  Stack<int> s;
+  assert(s.get_size() == 0);
+  s.push(100);
+  assert(s.get_top() == 100);
+  assert(s.get_size() == 1);
+  s.push(200);
+  assert(s.get_top() == 200);
+  assert(s.get_size() == 2);
+  assert(s.pop() == 200);
+  assert(s.get_size() == 1);
+  assert(s.pop() == 100);
+  assert(s.get_size() == 0);
+  s.push(300);
+  s.push(400);
+
+  Stack<int> t = oneTwoThree();
+
+  t = oneTwoThree();
+
+  s.pop();
+  cout << "All tests passed\n";
+}
